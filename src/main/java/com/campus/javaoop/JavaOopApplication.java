42ca8d67e3f03926d2b0c527d7bd9e33747a7ad5
kimeojin <kimaj2001@gmail.com>
2023-06-08 00:02:34 +0900

    Initialize java + gradle
    자바 예제

---
+++ src/main/java/com/campus/javaoop/JavaOopApplication.java
@@ -0,0 +1,13 @@
+package com.campus.javaoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOopApplication.class, args);
+	}
+
+}
