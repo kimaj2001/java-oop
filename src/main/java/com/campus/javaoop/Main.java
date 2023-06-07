dde11414909a12c9460fa8654cd5c351c838584e
kimeojin <kimaj2001@gmail.com>
2023-06-08 04:25:10 +0900

    Design #3 - Sort interface
    인터페이스 설계하고, 각 구현체가 인터페이스를 구현하는 구조로 수정

--- src/main/java/com/campus/javaoop/Main.java
+++ src/main/java/com/campus/javaoop/Main.java
@@ -1,7 +1,14 @@
 package com.campus.javaoop;
 
+import com.campus.javaoop.logic.BubbleSort;
+import com.campus.javaoop.logic.Sort;
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Sort<String> sort = new BubbleSort<>();
 
+        System.out.println("[reulst] " + sort.sort(Arrays.asList("1","2","5","3")));
     }
 }
